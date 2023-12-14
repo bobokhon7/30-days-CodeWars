4394ae60bddb39a1ee69457204095ab2a8af65c4
Bobokhon <rboboxon6055@gmail.com>
2023-12-15 08:46:15 +0900

    Day 3 is done

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-//return the total number of smiling faces in the array
-//Valid smiley face examples: :) :D ;-D :~)
-
-function countSmileys(arr) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (
-      arr[i] === ":)" ||
-      arr[i] === ":-)" ||
-      arr[i] === ":~)" ||
-      arr[i] === ";)" ||
-      arr[i] === ";-)" ||
-      arr[i] === ";~)"
-    ) {
-      newArr.push(arr[i]);
-    } else if (
-      arr[i] === ":D" ||
-      arr[i] === ";-D" ||
-      arr[i] === ";~D" ||
-      arr[i] === ":-D" ||
-      arr[i] === ";D" ||
-      arr[i] === ":~D"
-    ) {
-      newArr.push(arr[i]);
-    }
+function rentalCarCost(d) {
+  if (d >= 3 && d <= 6) {
+    return d * 40 - 20;
+  } else if (d >= 7) {
+    return d * 40 - 50;
+  } else {
+    return d * 40;
   }
-
-  return newArr.length;
 }
 
-console.log(countSmileys([";~)", ":)", ":-)", ":--)"]));
+console.log(rentalCarCost(7));
